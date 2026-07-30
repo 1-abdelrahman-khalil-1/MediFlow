8998f7d5ec356dd860fbf6ddc293929a59de1678
pavsoss <pavsafterdark@gmail.com>
2026-07-30 16:04:01 -0400

    fix: restore facility page imports dropped in merge

--- lib/main.dart
+++ lib/main.dart
@@ -21,17 +21,12 @@ import 'views/auth/forgot_password_page.dart';
 import 'views/auth/login_screen.dart';
 import 'views/auth/role_selection_screen.dart';
 import 'views/facility/active_indents_page.dart';
+import 'views/facility/ai_forecast_page.dart';
 import 'views/facility/daily_logging_page.dart';
+import 'views/facility/facility_overview.dart';
 import 'views/facility/wastage_report_page.dart';
 import 'views/facility/facility_profile_page.dart';
 import 'views/facility/alerts_hub_page.dart';
-
-// Admin Pages
-import 'views/admin/admin_overview.dart';
-import 'views/admin/admin_indent_approval_page.dart';
-import 'views/admin/admin_indent_status_page.dart';
-import 'views/admin/route_optimization_map.dart';
-import 'views/admin/audit_trail_page.dart';
 import 'views/shared/ai_chat_page.dart';
 import 'views/shared/help_page.dart';
 import 'views/shared/sidebar_layout.dart';
